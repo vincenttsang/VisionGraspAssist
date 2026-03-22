00e0b0ccf0872cf7887ad43b09a34594546ae556
Vincent Tsang <vincent_tsang2017@outlook.com>
2026-03-22 12:22:37 +0800

    Initial Commit

---
+++ VisionGraspAssist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisionGraspAssist
+//
+//  Created by Vincent Tsang on 22/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionGraspAssist/VisionGraspAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionGraspAssistApp.swift
+//  VisionGraspAssist
+//
+//  Created by Vincent Tsang on 22/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct VisionGraspAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
